d0405450998abdebe102769c9cd40073cc15a057
Miguel Angel Ramos <return.infox@gmail.com>
2024-04-22 21:33:06 -0400

    proyecto de grupos de usuarios2

--- accounts/signals.py
+++ accounts/signals.py
@@ -8,9 +8,10 @@ from .models import Profile
 def add_user_group(sender, instance, created, **kwargs):
     if created:
         try:
-            users_group = Group.objects.get(name='users')
+            users_group = Group.objects.get(name='admin')
         except Group.DoesNotExist:
-            users_group = Group.objects.create(name='users')
+            # users_group = Group.objects.create(name='users')
+            users_group = Group.objects.create(name='admin')
             
         instance.user.groups.add(users_group)
     
